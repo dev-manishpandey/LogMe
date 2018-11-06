fc59094348bed151ca3f6836e2160ee66a40b0d9
Manish Pandey <manishpandey@rarecrew.com>
2018-11-06 13:31:41 +0530

    First setup

---
+++ LogMe/LogMe/LogMe.h
@@ -0,0 +1,19 @@
+//
+//  LogMe.h
+//  LogMe
+//
+//  Created by Manish Pandey on 06/11/18.
+//  Copyright © 2018 Manish Pandey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LogMe.
+FOUNDATION_EXPORT double LogMeVersionNumber;
+
+//! Project version string for LogMe.
+FOUNDATION_EXPORT const unsigned char LogMeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LogMe/PublicHeader.h>
+
+
