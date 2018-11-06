139bd913f8d2ec35ff13a34ff8534c4286636bb0
Manish Pandey <manishpandey@rarecrew.com>
2018-11-06 14:19:44 +0530

    Setup new files

---
+++ Example/Pods/Target Support Files/LogMe/LogMe-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_LogMe : NSObject
+@end
+@implementation PodsDummy_LogMe
+@end
--- LogMe/LogMe/Service.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Service.swift
-//  LogMe
-//
-//  Created by Manish Pandey on 06/11/18.
-//  Copyright © 2018 Manish Pandey. All rights reserved.
-//
-
-import Foundation
